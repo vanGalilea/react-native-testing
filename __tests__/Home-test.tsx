a2d34dc44e6dd1bcabe969659a356569cd0e62bc
Piet Groot Kormelink <piet@mind4simple.nl>
2020-07-04 13:49:23 +0200

    solves failing test on my machine.

--- __tests__/Home-test.tsx
+++ __tests__/Home-test.tsx
@@ -1,14 +1,16 @@
 import 'react-native'
 import React from 'react'
 import {fireEvent, render, wait} from '@testing-library/react-native'
-import App from '../App'
+import App from '../src/components/App'
+//import Login from "../src/components/Login"
 
 //mocking async storage module
 jest.mock('@react-native-community/async-storage', () => ({setItem: jest.fn()}))
 
 
 it('renders/navigats throughout app screens', async () => {
-  const {getByText} = render(<App />)
+  const {getByText} = render(<App/>)
+  // const {getByText} = render(<Login onSubmit={() => {{}}} />)
   const homeText = getByText(/home/i)
   expect(homeText).not.toBeNull()
   fireEvent.press(getByText(/counter/i))
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,16 @@
 import 'react-native-gesture-handler'
 import React from 'react'
-import Counter from './src/components/Counter'
-import LoginSubmission from './src/components/LoginSubmission'
+import Counter from './Counter'
+import LoginSubmission from './LoginSubmission'
 import {NavigationContainer} from '@react-navigation/native'
 import {createStackNavigator} from '@react-navigation/stack'
-import EasyButton from './src/components/EasyButton'
-import Home from './src/components/Home'
-import {ThemeProvider} from './src/utils/theme'
+import EasyButton from './EasyButton'
+import Home from './Home'
+import {ThemeProvider} from '../utils/theme'
 
 const Stack = createStackNavigator()
 
-export const SCREENS = {HOME: "Home", COUNTER: "Counter", LOGIN: "Login", EASYBUTTON: "EasyButton"}
+export const SCREENS: Record<string,string> = {HOME: "Home", COUNTER: "Counter", LOGIN: "Login", EASYBUTTON: "EasyButton"}
 export default () => {
   return (
     <>
