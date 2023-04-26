141646f1db8e004fa662faa777285f19d702b82e
vanGalilea <galiliziv@gmail.com>
2023-04-26 21:14:37 +0200

    upgrade all android func.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,33 +1,32 @@
-import 'react-native-gesture-handler'
-import React from 'react'
-import Counter from './Counter'
-import LoginSubmission from './LoginSubmission'
-import {NavigationContainer} from '@react-navigation/native'
+import React from 'react';
+import Counter from './Counter';
+import LoginSubmission from './LoginSubmission';
+import {NavigationContainer} from '@react-navigation/native';
 import {
   createStackNavigator,
   StackNavigationProp,
-} from '@react-navigation/stack'
-import EasyButton from './EasyButton'
-import Home from './Home'
-import {ThemeProvider} from '../utils/theme'
-import Video from './Video'
-import Modal from './Modal'
-import FlatList from './FlatList'
-import ListWithFetch from './ListWithFetch'
+} from '@react-navigation/stack';
+import EasyButton from './EasyButton';
+import Home from './Home';
+import {ThemeProvider} from '../utils/theme';
+import Video from './Video';
+import Modal from './Modal';
+import FlatList from './FlatList';
+import ListWithFetch from './ListWithFetch';
 
 export type RootStackParamList = {
-  Home: undefined
-  Counter: undefined
-  Login: undefined
-  EasyButton: undefined
-  Video: undefined
-  Modal: undefined
-  FlatList: undefined
-  ListWithFetch: undefined
-}
-export type NavigationProps = StackNavigationProp<RootStackParamList>
+  Home: undefined;
+  Counter: undefined;
+  Login: undefined;
+  EasyButton: undefined;
+  Video: undefined;
+  Modal: undefined;
+  FlatList: undefined;
+  ListWithFetch: undefined;
+};
+export type NavigationProps = StackNavigationProp<RootStackParamList>;
 
-export const Stack = createStackNavigator<RootStackParamList>()
+export const Stack = createStackNavigator<RootStackParamList>();
 
 export const SCREENS: Record<string, keyof RootStackParamList> = {
   HOME: 'Home',
@@ -38,7 +37,7 @@ export const SCREENS: Record<string, keyof RootStackParamList> = {
   MODAL: 'Modal',
   FLATLIST: 'FlatList',
   LIST_WITH_FETCH: 'ListWithFetch',
-}
+};
 export default () => {
   return (
     <>
@@ -60,5 +59,5 @@ export default () => {
         </NavigationContainer>
       </ThemeProvider>
     </>
-  )
-}
+  );
+};
