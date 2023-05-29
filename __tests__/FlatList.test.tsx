2c4a04d62c10b29e25c23d6480b117790c377f20
vanGalilea <galiliziv@gmail.com>
2023-05-29 11:32:08 +0200

    FlatList revamp V

--- __tests__/FlatList.test.tsx
+++ __tests__/FlatList.test.tsx
@@ -1,48 +1,78 @@
-import React from 'react'
+import React from 'react';
 import {
+  act,
   cleanup,
   fireEvent,
   render,
   screen,
-  waitFor,
   waitForElementToBeRemoved,
-} from '@testing-library/react-native'
-import SectionList from '../src/components/FlatList'
+} from '@testing-library/react-native';
+import SectionList from '../src/components/FlatList';
 
+const SCREEN_SIZE = {
+  height: 500,
+  width: 300,
+};
 const eventData = {
   nativeEvent: {
     contentOffset: {
-      y: 500,
+      y: SCREEN_SIZE.height,
     },
-    contentSize: {
-      // Dimensions of the scrollable content
-      height: 500,
-      width: 100,
-    },
-    layoutMeasurement: {
-      // Dimensions of the device
-      height: 100,
-      width: 100,
-    },
+    // Dimensions of the scrollable content
+    contentSize: SCREEN_SIZE,
+    // Dimensions of the device
+    layoutMeasurement: SCREEN_SIZE,
   },
-}
+};
 
-afterEach(cleanup)
+afterEach(cleanup);
 
-it('scrolls to top and refreshes all items', async () => {
-  render(<SectionList />)
-  const {getByText, getByTestId} = screen
+it('scrolls to bottom and loads more items', async () => {
+  // Render the SectionList component
+  render(<SectionList />);
 
-  getByText(/pizza/i)
-  expect(() => getByText(/the impossible burger/i)).toThrow(
+  // First dish is visible
+  expect(screen.getByText(/pizza/i)).toBeOnTheScreen();
+  // First dish from 2nd page is no visible yet
+  expect(() => screen.getByText(/the impossible burger/i)).toThrow(
     'Unable to find an element with text: /the impossible burger/i',
-  ) //intially not shown
-  fireEvent.scroll(getByTestId('flat-list'), eventData)
-  await waitForElementToBeRemoved(() => getByText(/loading more dishes/i), {
-    timeout: 1500,
-  })
+  );
+  // We haven't started loading yet
+  expect(() => screen.getByText(/loading more dishes/i)).toThrow(
+    'Unable to find an element with text: /loading more dishes/i',
+  );
+  // Simulate scrolling to the bottom of the list
+  fireEvent.scroll(screen.getByLabelText('dishes-list'), eventData);
+  await waitForElementToBeRemoved(
+    () => screen.getByText(/loading more dishes/i),
+    {
+      timeout: 1500,
+    },
+  );
 
-  await waitFor(() => {
-    expect(getByText(/the impossible burger/i)).toBeTruthy()
+  expect(await screen.findByText(/the impossible burger/i)).toBeOnTheScreen();
+});
+
+it('refreshes when scrolling to the top', async () => {
+  // Render the SectionList component
+  render(<SectionList />);
+
+  // First dish is visible
+  expect(screen.getByText(/pizza/i)).toBeOnTheScreen();
+
+  // Simulate pull to refresh via refreshControl props, this can not be simulated by fireEvent.scroll
+  // See discussion https://github.com/callstack/react-native-testing-library/issues/809#issuecomment-984823700
+  const flatListTestInstance = screen.getByLabelText('dishes-list');
+  const {refreshControl} = flatListTestInstance.props;
+  await act(async () => {
+    refreshControl.props.onRefresh();
   });
-})
+
+  // First dish is not visible due to refresh and refreshing indicator is visible
+  expect(() => screen.getByText(/pizza/i)).toThrow(
+    'Unable to find an element with text: /pizza/i',
+  );
+  await waitForElementToBeRemoved(() => screen.getByText(/refreshing/i), {
+    timeout: 1500,
+  });
+});
