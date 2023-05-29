bb353ab48dea1c307958be90226643d93f406737
vanGalilea <galiliziv@gmail.com>
2023-05-29 11:40:52 +0200

    Home revamp V and assests transf.

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -4,7 +4,6 @@ import {
   fireEvent,
   render,
   screen,
-  waitFor,
 } from '@testing-library/react-native';
 import App from '../App';
 
@@ -16,15 +15,15 @@ jest.mock('@react-native-community/async-storage', () => ({
   setItem: mockedSetItem,
 }));
 
-it('renders/navigates throughout app screens', async () => {
+it('renders/navigates throughout app screens', () => {
+  // Render the app from teh root
   render(<App />);
-  const {getByText} = screen;
-  const homeText = getByText(/home/i);
-  expect(homeText).not.toBeNull();
-  fireEvent.press(getByText(/counter/i));
 
-  await waitFor(() => {
-    const counterText = getByText(/Current count:/i);
-    expect(counterText.props.children).toEqual(['Current count: ', 0]);
-  });
+  // Check whether we're in the home screen
+  expect(screen.getByText(/home/i)).toBeOnTheScreen();
+
+  // Navigate to counter screen by pressing on button
+  fireEvent.press(screen.getByText(/counter/i));
+  // Check that navigation was succeeded by inspecting correspondeing text on the screen
+  expect(screen.getByText(/current count: 0/i)).toBeOnTheScreen();
 });
