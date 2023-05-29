e61a4db77ed96d9866969798b8f02246ec80767e
vanGalilea <galiliziv@gmail.com>
2023-05-29 10:31:02 +0200

    count with hook case V

--- src/hooks/useCounter.ts
+++ src/hooks/useCounter.ts
@@ -1,8 +1,21 @@
-import React from 'react'
+import {useState} from 'react';
 
-export default ({initialCount = 0, step = 1} = {}) => {
-  const [count, setCount] = React.useState(initialCount)
-  const increment = () => setCount(c => c + step)
-  const decrement = () => setCount(c => c - step)
-  return {count, increment, decrement}
+export interface IUseCounterProps {
+  initialCount?: number;
+  step?: number;
+}
+
+export interface IUseCounterResult {
+  count: number;
+  increment(): void;
+  decrement(): void;
+}
+export default ({
+  initialCount = 0,
+  step = 1,
+}: IUseCounterProps = {}): IUseCounterResult => {
+  const [count, setCount] = useState<number>(initialCount);
+  const increment = () => setCount(c => c + step);
+  const decrement = () => setCount(c => c - step);
+  return {count, increment, decrement};
 };
