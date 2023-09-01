aac4521650250185a4cbddbdacbffec043aa7ebb
vanGalilea <galiliziv@gmail.com>
2023-09-01 10:47:47 +0200

    rn upgrade & maestro e2e tests

---
+++ e2e/setup.js
@@ -0,0 +1,37 @@
+output.screens = {
+  navigation: {
+    goBackHomeButtonTestID: 'go-back-home-button',
+  },
+  home: {
+    title: 'Go to component...',
+    counterButton: 'Counter',
+    loginButton: 'Login',
+    easyButtonButton: 'EasyButton',
+    videoButton: 'video',
+  },
+  counter: {
+    currentCountIs0: 'Current count: 0',
+    currentCountIs1: 'Current count: 1',
+    incrementButton: 'Increment',
+    decrementButton: 'Decrement',
+  },
+  login: {
+    usernamePlaceholder: 'Username',
+    passwordPlaceholder: 'Password',
+    username: 'admin',
+    password: 'admin',
+    submitButton: 'Submit',
+    loadingIsVisible: 'loading...',
+  },
+  easyButton: {
+    button: 'Click me!',
+    modalDescription: 'You clicked me!',
+    modalDismissButton: 'OK',
+  },
+  video: {
+    fullScreenButton: 'Full screen',
+    exitFullScreen: 'Exit full screen',
+    pauseStartButton: 'Pause/Start',
+    playerA11yId: 'video-player',
+  },
+};
